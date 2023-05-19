054008fbbeede084fcdf63ce36fa1c61c6420cee
Ahmad Khoirul Umam <126857158+AKhoirulUmam@users.noreply.github.com>
2023-05-19 07:10:00 -0700

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,18 @@
+create database users;
+show database;
+
+use users;
+
+create table users(nama varchar(30),email varchar(30),address varchar(30));
+
+insert into users (nama,email,address)values("umam","umam0800@gmail.com","Bogor");
+
+select *from users;
+
+show tables;
+
+desc table users;
+
+
+
+
