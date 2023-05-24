2673415cece9af17be53e2d429a76fb45028dde6
Ahmad Khoirul Umam <126857158+AKhoirulUmam@users.noreply.github.com>
2023-05-23 19:50:38 -0700

    Update join.sql

--- join.sql
+++ join.sql
@@ -1,3 +1,12 @@
-SELECT userid FROM table1 LEFT JOIN table2 LEFT JOIN table3 ON table1.userid= table2.productid = table3.categoriesid;
+SELECT
+     user_id,
+     product_id,
+     categories_id
+FROM
+    user id
+LEFT JOIN product id ON product_id.user_id = user_id.user.id
+LEFT JOIN categories id ON categories_id.product_id = product_id.product_id
+WHERE product_id.product_id;
 
-SELECT user.userid, product.productid, categories.categoriesid FROM user LEFT JOIN order LEFT JOIN categories ON user.userid = product.productid = categories.categoriesid ORDER BY user.userinteger;
+
+
